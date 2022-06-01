a715f077e777420d20e0e72533857139d5b0b940
Akindele Daniel <danielakindele8@gmail.com>
2022-06-01 03:44:28 +0100

    Added a new project

--- src/Components/Feducation.js
+++ src/Components/Feducation.js
@@ -8,104 +8,34 @@ const Feducation = () => {
                 <h1>Formal Education</h1>
             </div>
             <div className='tierCon'>
-
-          
-                    <div className="tier">
-                        <h4>Tertiary Education</h4>
-                        <span>Programme:Bachelor of Engineering</span>
-                            <span>Discipline:Aerospace Engineering(Aeronautics)</span>
-                        <p>
-                            <span>Institute:</span>
-                            Air Force Institute of Technology,Kaduna</p>
-                        <p>
-                            <span>Duration:</span>
-                            2020 - 2025
-                        </p>
-                    </div>
-                    <div className="tier">
-                        <h4>Tertiary Education</h4>
-                        <span>Programme:Bachelor of Engineering</span>
-                            <span>Discipline:Aerospace Engineering(Aeronautics)</span>
-                        <p>
-                            <span>Institute:</span>
-                            Air Force Institute of Technology,Kaduna</p>
-                        <p>
-                            <span>Duration:</span>
-                            2020 - 2025
-                        </p>
-                    </div>
-                    <div className="tier">
-                        <h4>Tertiary Education</h4>
-                        <span>Programme:Bachelor of Engineering</span>
-                            <span>Discipline:Aerospace Engineering(Aeronautics)</span>
-                        <p>
-                            <span>Institute:</span>
-                            Air Force Institute of Technology,Kaduna</p>
-                        <p>
-                            <span>Duration:</span>
-                            2020 - 2025
-                        </p>
-                    </div>
-                    <div className="tier">
-                        <h4>Tertiary Education</h4>
-                        <span>Programme:Bachelor of Engineering</span>
-                            <span>Discipline:Aerospace Engineering(Aeronautics)</span>
-                        <p>
-                            <span>Institute:</span>
-                            Air Force Institute of Technology,Kaduna</p>
-                        <p>
-                            <span>Duration:</span>
-                            2020 - 2025
-                        </p>
-                    </div>
-                    <div className="tier">
-                        <h4>Tertiary Education</h4>
-                        <span>Programme:Bachelor of Engineering</span>
-                            <span>Discipline:Aerospace Engineering(Aeronautics)</span>
-                        <p>
-                            <span>Institute:</span>
-                            Air Force Institute of Technology,Kaduna</p>
-                        <p>
-                            <span>Duration:</span>
-                            2020 - 2025
-                        </p>
-                    </div>
-                    <div className="tier">
-                        <h4>Tertiary Education</h4>
-                        <span>Programme:Bachelor of Engineering</span>
-                            <span>Discipline:Aerospace Engineering(Aeronautics)</span>
-                        <p>
-                            <span>Institute:</span>
-                            Air Force Institute of Technology,Kaduna</p>
-                        <p>
-                            <span>Duration:</span>
-                            2020 - 2025
-                        </p>
-                    </div>
-                    <div className="tier">
-                        <h4>Tertiary Education</h4>
-                        <span>Programme:Bachelor of Engineering</span>
-                            <span>Discipline:Aerospace Engineering(Aeronautics)</span>
-                        <p>
-                            <span>Institute:</span>
-                            Air Force Institute of Technology,Kaduna</p>
-                        <p>
-                            <span>Duration:</span>
-                            2020 - 2025
-                        </p>
-                    </div>
-                    <div className="tier">
-                        <h4>Tertiary Education</h4>
-                        <span>Programme:Bachelor of Engineering</span>
-                            <span>Discipline:Aerospace Engineering(Aeronautics)</span>
-                        <p>
-                            <span>Institute:</span>
-                            Air Force Institute of Technology,Kaduna</p>
-                        <p>
-                            <span>Duration:</span>
-                            2020 - 2025
-                        </p>
-                    </div>
+                <div className="tier">
+                    <h4>Tertiary Education</h4>
+                    <p><span>Programme</span>Bachelor of Engineering(In view)</p>
+                    <p><span>Discipline</span>Aerospace Engineering(Aeronautics)</p>
+                    <p><span>Institute</span>Air Force Institute of Technology,Kaduna</p>
+                    <p><span>Duration</span>2020 - 2025</p>
+                </div>
+                <div className="tier">
+                    <h4>Secondary Education</h4>
+                    <p><span>Programme</span>Senior Sch Certificate</p>
+                    <p><span>Discipline</span>Sciences</p>
+                    <p><span>Institute</span>Enny Dav College,Olambe.</p>
+                    <p><span>Duration</span>2018</p>
+                </div>
+                <div className="tier">
+                    <h4>Junior Secondary Education</h4>
+                    <p><span>Programme</span>Junior Secondary Sch Certificate</p>
+                    <p><span>Discipline</span>NIL</p>
+                    <p><span>Institute</span>Nolad City College</p>
+                    <p><span>Duration</span>2014</p>
+                </div>
+                <div className="tier">
+                    <h4>Primary School Education</h4>
+                    <p><span>Programme</span>First School Leaving Certificate</p>
+                    <p><span>Discipline</span>NIL</p>
+                    <p><span>Institute</span>Nolad City Nursery and Primary School,Osere.</p>
+                    <p><span>Duration</span>2020 - 2025</p>
+                </div>
             </div>
 
         </div>
